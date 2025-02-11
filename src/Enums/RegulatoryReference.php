9bbced3908b9e7ade59d53ea26f604744c687d98
Markus Kleiton <102915789+MineStoreCMS@users.noreply.github.com>
2025-02-11 23:22:26 +0200

    Update RegulatoryReference.php

--- src/Enums/RegulatoryReference.php
+++ src/Enums/RegulatoryReference.php
@@ -6,6 +6,7 @@ use BenSampo\Enum\Enum;
 
 /**
  * @property string $value
+ * @method static self N2_2() Non soggette - altri casi
  * @method static self N3_3() Non Imponibile cessioni verso San Marino e Citta del Vaticano
  */
 final class RegulatoryReference extends Enum
